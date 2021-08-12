1c0260137f6f9846e310de4d7bb2ac04bcff6223
Valeran <valeran.maytie@protonmail.com>
2021-08-12 18:02:34 +0200

    base du bot

---
+++ rp.py
@@ -0,0 +1,27 @@
+import discord
+
+from config import config
+from voice import voic_event_traitment, voice_commande
+
+client = discord.Client()
+
+
+@client.event
+async def on_message(message):
+    if message.author.id != client.user.id:
+        if message.content.startswith("n!voice"):
+            reponse = await voice_commande(client, message)
+            if reponse is not None and reponse != "":
+                await message.channel.send(reponse)
+
+
+@client.event
+async def on_voice_state_update(member, before, after):
+    await voic_event_traitment(member, before, after, client)
+
+
+@client.event
+async def on_ready():
+    print("Logged as {}!".format(client.user))
+
+client.run(config.token)
